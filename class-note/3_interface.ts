c39e4eeea03388d2f5baffae8194ca19dd8f5085
bellDev-code <sumaoo20@naver.com>
2022-03-25 00:36:44 +0900

    TypeScript todolist & interface

---
+++ class-note/3_interface.ts
@@ -0,0 +1,9 @@
+interface User {
+    age: number;
+    name: string;
+}
+
+const lee: User = {
+    age: 28,
+    name: 'jongho'
+}
--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,6 +1,12 @@
-let todoItems: object[];
+type Todos = {
+    id: number;
+    title: string;
+    done: boolean;
+}
 
-function fetchTodoItems(): object[] {
+let todoItems: Todos[];
+
+function fetchTodoItems(): Todos[] {
     const todos = [
         { id: 1, title: '안녕', done: false},
         { id: 2, title: '타입', done: false},
@@ -10,44 +16,63 @@ function fetchTodoItems(): object[] {
 }
 
 // crud methods
-function fetchTodos(): object[] {
+function fetchTodos(): Todos[] {
     const todos = fetchTodoItems();
     return todos
 }
 
-function addTodo(todo: object): void {
+function addTodo(todo: Todos) {
     todoItems.push(todo)
 }
 
-function deleteTodo(index: number): void {
+function deleteTodo(index: number) {
     todoItems.splice(index, 1)
 }
 
-function completeTodo(index: number, todo: object): void {
+function completeTodo(index: number, todo: Todos) {
     todo.done = true
     todoItems.splice(index, 1, todo)
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
     return todoItems[0]
 }
 
-function showCompleted() {
-    return todoItems.filter(item => item.done)
+function showCompleted(): Todos[] {
+    // return todoItems.filter(item => item.done)
+    return todoItems.filter(function(item) {
+        if(item.done) {
+            return item
+        }
+    })
 }
 
 // @TODO: 아래 함수의 내용을 채워보세요
-function addTwodoItems() {
+function addTwodoItems(): void {
     // addTodo() 함수를 두 번 호출하여 todoItems에 새 할일이 2개 추가 되어야합니다.
-    addTodo("1")
-    addTodo("2")
+    addTodo({
+        id: 4,
+        title: '아이템',
+        done: false
+    })
+    addTodo({
+        id: 5,
+        title: '흐에',
+        done: false
+    })
 }
 
-function log() {
+function log(): void {
+    completeTodo(6, {id: 6, title: 'coding', done: true})
+    completeTodo(7, {id: 7, title: 'boxing', done: true})
+
+    
+
     console.log(todoItems)
 }
 
 todoItems = fetchTodoItems()
 addTwodoItems()
+showCompleted()
 log()
